3578c5f22d4bc3b807499806b88bd4e87f0e90b5
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2022-03-27 16:54:56 +0600

    Time: 249 ms (20.85%), Space: 47.1 MB (69.35%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,28 @@
+var romanToInt = function(s) {
+
+    const sym = { 
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    
+    let result = 0;
+    for (i=0; i < s.length; i++){
+        const current = sym[s[i]];
+        const next = sym[s[i+1]];
+
+        if (current < next){
+            result += next - current
+            i++
+        } else {
+            result += current
+        }
+    }
+
+    return result; 
+};
