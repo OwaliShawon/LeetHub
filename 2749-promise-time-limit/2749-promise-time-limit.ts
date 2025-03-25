83d139a619f3067bf43c740c47dbe79b9a1e697c
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2025-03-25 12:37:57 +0600

    Time: 43 ms (96.44%), Space: 56.2 MB (20.76%) - LeetHub

--- 2749-promise-time-limit/2749-promise-time-limit.ts
+++ 2749-promise-time-limit/2749-promise-time-limit.ts
@@ -1,26 +1,18 @@
-type Fn = (...params: any[]) => Promise<any>;
-
-function timeLimit(fn: Fn, t: number): Fn {
-  return async function (...args: any[]) {
-    return new Promise((delayresolve, reject) => {
-      const timeoutId = setTimeout(() => {
-        clearTimeout(timeoutId);
-        reject("Time Limit Exceeded");
-      }, t);
-
-      fn(...args)
-        .then((result) => {
-          clearTimeout(timeoutId);
-          delayresolve(result);
-        })
-        .catch((error) => {
-          clearTimeout(timeoutId);
-          reject(error);
-        });
-    });
-  };
-}
 /**
- * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
- * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
  */
+var timeLimit = function (fn, t) {
+    return async function (...args) {
+        const originalFnPromise = fn(...args);
+
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject('Time Limit Exceeded')
+            }, t);
+        })
+
+        return Promise.race([originalFnPromise, timeoutPromise]);
+    }
+};
