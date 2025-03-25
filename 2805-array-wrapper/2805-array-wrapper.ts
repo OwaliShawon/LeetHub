120254e3dd542ea42e74d4982efa9129d1fdcc1b
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2025-03-25 11:06:42 +0600

    Time: 49 ms (69.63%), Space: 57 MB (36.52%) - LeetHub

---
+++ 2805-array-wrapper/2805-array-wrapper.ts
@@ -0,0 +1,22 @@
+class ArrayWrapper {
+    array = [];
+    constructor(nums: number[]) {
+        this.array = nums;
+    }
+    
+    valueOf(): number {
+        return this.array.reduce((sum, num) => sum + num, 0);
+    }
+    
+    toString(): string {
+        return "[" + this.array.join(",") + "]";
+    }
+};
+
+/**
+ * const obj1 = new ArrayWrapper([1,2]);
+ * const obj2 = new ArrayWrapper([3,4]);
+ * obj1 + obj2; // 10
+ * String(obj1); // "[1,2]"
+ * String(obj2); // "[3,4]"
+ */
